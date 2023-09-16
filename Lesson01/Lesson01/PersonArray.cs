eca398e534c139adcb8810cca23b752c7805ffb1
Miraziz.Khidoyatov <49793609+Mirazyzz@users.noreply.github.com>
2023-09-16 22:08:18 +0500

    Class exercises

---
+++ Lesson01/Lesson01/PersonArray.cs
@@ -0,0 +1,45 @@
+﻿namespace Lesson01
+{
+    internal class PersonArray
+    {
+        private Person[] _array = new Person[10];
+        private int currentIndex = 0;
+
+        public void Add(Person number)
+        {
+            if (currentIndex >= _array.Length)
+            {
+                Person[] newArray = new Person[_array.Length * 2];
+                for (int i = 0; i < _array.Length; i++)
+                {
+                    newArray[i] = _array[i];
+                }
+                _array = newArray;
+            }
+
+            _array[currentIndex++] = number;
+        }
+
+        public void Remove(Person number)
+        {
+            for (int i = 0; i < _array.Length; i++)
+            {
+                if (_array[i] == number)
+                {
+                    _array[i] = null;
+                    break;
+                }
+            }
+        }
+
+        public void DisplayAll()
+        {
+            foreach (Person number in _array)
+            {
+                Console.Write($"{number} ");
+            }
+
+            Console.WriteLine();
+        }
+    }
+}
---
+++ Lesson01/Lesson01/StringArray.cs
@@ -0,0 +1,45 @@
+﻿namespace Lesson01
+{
+    internal class StringArray
+    {
+        private string[] _array = new string[10];
+        private int currentIndex = 0;
+
+        public void Add(string number)
+        {
+            if (currentIndex >= _array.Length)
+            {
+                string[] newArray = new string[_array.Length * 2];
+                for (int i = 0; i < _array.Length; i++)
+                {
+                    newArray[i] = _array[i];
+                }
+                _array = newArray;
+            }
+
+            _array[currentIndex++] = number;
+        }
+
+        public void Remove(string number)
+        {
+            for (int i = 0; i < _array.Length; i++)
+            {
+                if (_array[i] == number)
+                {
+                    _array[i] = null;
+                    break;
+                }
+            }
+        }
+
+        public void DisplayAll()
+        {
+            foreach (string number in _array)
+            {
+                Console.Write($"{number} ");
+            }
+
+            Console.WriteLine();
+        }
+    }
+}
